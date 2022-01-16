d0f092d17390f321cad4f9f1c0a9f845679e1026
Haris Mlačo <haris.mlaco@edu.fit.ba>
2022-01-16 10:07:04 +0100

    Added login screen and removed unnecesary screens

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,9 +1,139 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:developer';
 
-class LoginScreen extends StatelessWidget {
+import 'package:esnflutter/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+
+class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+Future<http.Response> Login(String email, String password) {
+  return http.put(
+    Uri.parse('https://10.0.2.2:8012/User/login'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{
+      'username': email,
+      'password': password,
+    }),
+  );
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final email = TextEditingController();
+  final password = TextEditingController();
+  bool _passwordVisible = false;
+  @override
+  void initState() {
+    _passwordVisible = false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Column(
+        children: [
+          Center(
+            child: Container(
+              padding: EdgeInsets.only(top: 30),
+              height: 100,
+              child: Text(
+                "Login",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 25,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              keyboardType: TextInputType.text,
+              controller: email,
+              decoration: InputDecoration(
+                labelText: 'Email',
+                hintText: 'Unesite vaš email',
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(width: 3, color: Colors.blue),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(width: 3, color: Colors.red),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: TextFormField(
+              keyboardType: TextInputType.text,
+              controller: password,
+              obscureText: !_passwordVisible,
+              decoration: InputDecoration(
+                labelText: 'Lozinka',
+                hintText: 'Unesite vašu lozinku',
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(width: 3, color: Colors.blue),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(width: 3, color: Colors.red),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    _passwordVisible ? Icons.visibility : Icons.visibility_off,
+                    color: Colors.black,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _passwordVisible = !_passwordVisible;
+                    });
+                  },
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Center(
+              child: InkWell(
+                onTap: () async {
+                  var response = await Login(email.text, password.text);
+                  if (response.body.contains("true")) {
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => HomeScreen()));
+                  }
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.black,
+                  ),
+                  width: 100,
+                  height: 50,
+                  child: Center(
+                    child: Text(
+                      "Potvrdi",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
